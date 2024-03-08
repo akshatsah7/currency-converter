3fe97fd65babda12ce763149b6628e91f907f664
akshatsah7 <akshatsah63@gmail.com>
2024-03-08 13:47:06 +0530

    made code visibility better through better indentation

--- index.js
+++ index.js
@@ -8,47 +8,59 @@ const exRateTxt = document.querySelector("form .result");
 // Event listener for currency dropdowns (select)
 
 [fromCur, toCur].forEach((select, i) => {
-    for (let curCode in Country_List) {
-        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "INR") ? "selected" : "";
-        select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
-    }
-    select.addEventListener("change", () => {
-        const code = select.value;
-        const imgTag = select.parentElement.querySelector("img");
-        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
-    });
+  for (let curCode in Country_List) {
+    const selected =
+      (i === 0 && curCode === "USD") || (i === 1 && curCode === "INR")
+        ? "selected"
+        : "";
+    select.insertAdjacentHTML(
+      "beforeend",
+      `<option value="${curCode}" ${selected}>${curCode}</option>`
+    );
+  }
+  select.addEventListener("change", () => {
+    const code = select.value;
+    const imgTag = select.parentElement.querySelector("img");
+    imgTag.src = `https://flagcdn.com/48x36/${Country_List[
+      code
+    ].toLowerCase()}.png`;
+  });
 });
 
 // Function to get exchange rate from api
 
 async function getExchangeRate() {
-    const amountVal = amount.value || 1;
-    exRateTxt.innerText = "Getting exchange rate...";
-    try {
-        const response = await fetch(`https://v6.exchangerate-api.com/v6/df81828522b806b3fb1d3331/latest/${fromCur.value}`);
-        const result = await response.json();
-        const exchangeRate = result.conversion_rates[toCur.value];
-        const totalExRate = (amountVal * exchangeRate).toFixed(2);
-        exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
-    } catch (error) {
-        exRateTxt.innerText = "Something went wrong...";
-    }
+  const amountVal = amount.value || 1;
+  exRateTxt.innerText = "Getting exchange rate...";
+  try {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/df81828522b806b3fb1d3331/latest/${fromCur.value}`
+    );
+    const result = await response.json();
+    const exchangeRate = result.conversion_rates[toCur.value];
+    const totalExRate = (amountVal * exchangeRate).toFixed(2);
+    exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
+  } catch (error) {
+    exRateTxt.innerText = "Something went wrong...";
+  }
 }
 
 // Event listeners for button and exchange icon click
 
 window.addEventListener("load", getExchangeRate);
 getBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    getExchangeRate();
+  e.preventDefault();
+  getExchangeRate();
 });
 
 exIcon.addEventListener("click", () => {
-    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
-    [fromCur, toCur].forEach((select) => {
-        const code = select.value;
-        const imgTag = select.parentElement.querySelector("img");
-        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
-    });
-    getExchangeRate();
+  [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
+  [fromCur, toCur].forEach((select) => {
+    const code = select.value;
+    const imgTag = select.parentElement.querySelector("img");
+    imgTag.src = `https://flagcdn.com/48x36/${Country_List[
+      code
+    ].toLowerCase()}.png`;
+  });
+  getExchangeRate();
 });
